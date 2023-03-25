b20d9c26e2ab0aa98bb22075fa3a3f1b7f24a5b7
RihardXXX <yusupa.akaeva@yandex.ru>
2023-03-25 20:29:50 +0300

    первичная инициализация проекта лендоса моего профиля

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <header>
+      <Nuxt-link to="/"> Home </Nuxt-link>
+      <Nuxt-link to="/about"> About </Nuxt-link>
+    </header>
+    <main>
+      <Nuxt />
+    </main>
+  </div>
+</template>
