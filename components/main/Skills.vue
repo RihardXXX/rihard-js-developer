126b6f1890488c41685f05e3bf0c53d16beaa4d3
RihardXXX <yusupa.akaeva@yandex.ru>
2023-04-02 10:14:52 +0300

    верстка карточки навыков

--- components/main/Skills.vue
+++ components/main/Skills.vue
@@ -12,49 +12,156 @@
     </div>
     <div :class="['swiper', $style.swiperWrap]">
       <!-- Additional required wrapper -->
-      <div class="swiper-wrapper">
+      <div v-if="Boolean(slides.length)" class="swiper-wrapper">
         <!-- Slides -->
-        <div :class="['swiper-slide', $style.sliderItem]">
-          <article :class="$style.card">
-            <div :class="$style.head">
-              <SvgIcon name="js" size="big10" />
-            </div>
-            <div :class="$style.body">description</div>
-          </article>
-          <!-- <article :class="$style.card">card1</article>
-          <article :class="$style.card">card1</article>
-          <article :class="$style.card">card1</article> -->
+        <div
+          v-for="slide in slides"
+          :key="slide.id"
+          :class="['swiper-slide', $style.sliderItem]"
+        >
+          <VCardSkills
+            v-for="card in slide.cards"
+            :key="card.id"
+            :svg-name="card.svgName"
+            :title="card.title"
+            :description="card.description"
+          />
         </div>
       </div>
+
       <!-- If we need pagination -->
       <div class="swiper-pagination"></div>
 
       <!-- If we need navigation buttons -->
-      <div class="swiper-button-prev"></div>
-      <div class="swiper-button-next"></div>
+      <!-- <div class="swiper-button-prev"></div>
+      <div class="swiper-button-next"></div> -->
 
       <!-- If we need scrollbar -->
-      <div class="swiper-scrollbar"></div>
+      <!-- <div class="swiper-scrollbar"></div> -->
     </div>
   </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import SvgIcon from '@/components/ui/VIcon.vue';
 import VInfo from '@/components/VInfo.vue';
+import VCardSkills from '@/components/skills/VCardSkills.vue';
+
+let idx = 0;
+
+interface ICardSkill {
+  id: string;
+  svgName: string;
+  title: string;
+  description: string;
+}
+
+interface ISlideSkill {
+  id: string;
+  cards: Array<ICardSkill>;
+}
 
 export default defineComponent({
   name: 'SkillsPage',
 
   components: {
     VInfo,
-    SvgIcon,
+    VCardSkills,
   },
 
   data() {
     return {
       swiper: {},
+
+      slides: [
+        {
+          id: `slideItem${idx++}`,
+          cards: [
+            {
+              id: `cardItem${idx++}`,
+              svgName: 'js',
+              title: 'Javascript',
+              description: 'Разработка высоко-нагруженных приложений на JS',
+            },
+            {
+              id: `cardItem${idx++}`,
+              svgName: 'js',
+              title: 'Javascript',
+              description: 'Разработка высоко-нагруженных приложений на JS',
+            },
+            {
+              id: `cardItem${idx++}`,
+              svgName: 'js',
+              title: 'Javascript',
+              description: 'Разработка высоко-нагруженных приложений на JS',
+            },
+            {
+              id: `cardItem${idx++}`,
+              svgName: 'js',
+              title: 'Javascript',
+              description: 'Разработка высоко-нагруженных приложений на JS',
+            },
+          ],
+        },
+        {
+          id: `slideItem${idx++}`,
+          cards: [
+            {
+              id: `cardItem${idx++}`,
+              svgName: 'js',
+              title: 'Javascript',
+              description: 'Разработка высоко-нагруженных приложений на JS',
+            },
+            {
+              id: `cardItem${idx++}`,
+              svgName: 'js',
+              title: 'Javascript',
+              description: 'Разработка высоко-нагруженных приложений на JS',
+            },
+            {
+              id: `cardItem${idx++}`,
+              svgName: 'js',
+              title: 'Javascript',
+              description: 'Разработка высоко-нагруженных приложений на JS',
+            },
+            {
+              id: `cardItem${idx++}`,
+              svgName: 'js',
+              title: 'Javascript',
+              description: 'Разработка высоко-нагруженных приложений на JS',
+            },
+          ],
+        },
+        {
+          id: `slideItem${idx++}`,
+          cards: [
+            {
+              id: `cardItem${idx++}`,
+              svgName: 'js',
+              title: 'Javascript',
+              description: 'Разработка высоко-нагруженных приложений на JS',
+            },
+            {
+              id: `cardItem${idx++}`,
+              svgName: 'js',
+              title: 'Javascript',
+              description: 'Разработка высоко-нагруженных приложений на JS',
+            },
+            {
+              id: `cardItem${idx++}`,
+              svgName: 'js',
+              title: 'Javascript',
+              description: 'Разработка высоко-нагруженных приложений на JS',
+            },
+            {
+              id: `cardItem${idx++}`,
+              svgName: 'js',
+              title: 'Javascript',
+              description: 'Разработка высоко-нагруженных приложений на JS',
+            },
+          ],
+        },
+      ] as Array<ISlideSkill>,
     };
   },
 
@@ -97,38 +204,26 @@ export default defineComponent({
   }
 
   .swiperWrap {
-    border: 1px solid black;
+    /* border: 1px solid black; */
     width: 50%;
     height: 100%;
     min-height: 35rem;
+    transition: all 1s;
 
     .sliderItem {
+      width: 30rem;
       display: flex;
       justify-content: space-between;
       align-items: center;
       gap: 4rem;
       flex-wrap: wrap;
-
-      .card {
-        width: 25.5rem;
-        height: 16rem;
-        border: 1px solid royalblue;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: space-between;
-
-        .head {
-          display: flex;
-          align-items: center;
-          justify-content: space-between;
-
-          /* .test {
-            width: 2rem;
-          } */
-        }
-      }
+      height: 100%;
     }
+
+    /* &:hover {
+      transform: perspective(600px) rotateY(-20deg);
+      box-shadow: 0.1rem 0.1rem 0.5rem 0.1rem $gray;
+    } */
   }
 }
 </style>
---
+++ components/skills/VCardSkills.vue
@@ -0,0 +1,77 @@
+<template>
+  <article :class="$style.card">
+    <div :class="$style.head">
+      <VIcon :name="svgName" size="big7" />
+      <h3 :class="$style.title">{{ title }}</h3>
+    </div>
+    <div :class="$style.body">
+      {{ description }}
+    </div>
+  </article>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import VIcon from '@/components/ui/VIcon.vue';
+
+export default defineComponent({
+  name: 'VCardSkills',
+
+  components: {
+    VIcon,
+  },
+
+  props: {
+    svgName: {
+      type: String as PropType<String>,
+      required: true,
+    },
+
+    title: {
+      type: String as PropType<String>,
+      required: true,
+    },
+
+    description: {
+      type: String as PropType<String>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style lang="scss" module>
+.card {
+  width: 25.5rem;
+  /* height: 16rem; */
+  /* border: 1px solid royalblue; */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding: 2.1rem 1.8rem;
+
+  .head {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    width: 100%;
+    height: 6.4rem;
+
+    .title {
+      font-weight: 600;
+      font-size: 2rem;
+      line-height: 2rem;
+      color: $black;
+      margin-left: 1.5rem;
+    }
+  }
+
+  .body {
+    margin-top: 1.2rem;
+    font-size: 1.4rem;
+    line-height: 150%;
+    color: $gray-100;
+  }
+}
+</style>
