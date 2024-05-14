2bf843d917bc45c1c6314afbcb5219fe5ca6f6fe
RihardXXX <yusupa.akaeva@yandex.ru>
2024-05-14 20:45:47 +0300

    resolve conflicts

--- dist/_nuxt/static/1682695243/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Frihard-js-developer\u002F_nuxt\u002Fstatic\u002F1682695243",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Frihard-js-developer\u002F",assetsPath:"\u002Frihard-js-developer\u002F_nuxt\u002F",cdnURL:a}}}}(null));
